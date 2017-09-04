ea9dfc2adbf93d394d99912c06878fa46ab3433e
choi <chl8263@naver.com>
2017-09-04 11:28:04 +0900

    노트북

--- app/src/main/java/com/example/user/mcfm/Fragment/SecondFragment.java
+++ app/src/main/java/com/example/user/mcfm/Fragment/SecondFragment.java
@@ -6,21 +6,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.user.mcfm.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import static android.R.id.list;
 
 /**
  * Created by User on 2017-08-20.
  */
 
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener{
+    private ListView listView;
+    private EditText editText;
+    private ImageButton imageButton;
+    private ArrayAdapter arrayAdapter;
+    private String name = "Guest";
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("message");
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.second,container,false);
+        listView = (ListView) layout.findViewById(R.id.list);
+        editText = (EditText)layout.findViewById(R.id.chatinput);
+        imageButton = (ImageButton)layout.findViewById(R.id.chatinputBtn);
+        imageButton.setOnClickListener((View.OnClickListener) getActivity().getApplicationContext());
+        arrayAdapter = new ArrayAdapter<String>(layout.getContext(),android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
 
         return layout;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.chatinputBtn:
+                HashMap<String,Object> map = new HashMap<String,Object>();
+
+                String key = reference.push().getKey();
+                reference.updateChildren(map);
+
+                DatabaseReference root = reference.child(key);
+
+
+                break;
+        }
+
+    }
 }
--- app/src/main/java/com/example/user/mcfm/MainActivity.java
+++ app/src/main/java/com/example/user/mcfm/MainActivity.java
@@ -3,29 +3,33 @@ package com.example.user.mcfm;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.user.mcfm.Fragment.FirstFragment;
 import com.example.user.mcfm.Fragment.FourthFragment;
 import com.example.user.mcfm.Fragment.SecondFragment;
 import com.example.user.mcfm.Fragment.ThirdFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button people, chat, three, four;
+public class MainActivity extends AppCompatActivity{
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setInit();
+        InitViewPager();
+        InitStatusbar();
+
+    }
+    private void InitStatusbar(){
         View view = getWindow().getDecorView();
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             if(view!=null){
@@ -34,38 +38,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }else getWindow().setStatusBarColor(Color.parseColor("#000"));
     }
-
-    private void setInit() {
-        people = (Button) findViewById(R.id.people);
-        people.setOnClickListener(this);
-        chat = (Button) findViewById(R.id.chat);
-        chat.setOnClickListener(this);
-        three = (Button) findViewById(R.id.three);
-        three.setOnClickListener(this);
-        four = (Button) findViewById(R.id.four);
-        four.setOnClickListener(this);
+    private void InitViewPager() {
         viewPager = (ViewPager)findViewById(R.id.ViewPager);
         pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
-        viewPager.setCurrentItem(0);
-    }
+        tabLayout = (TabLayout)findViewById(R.id.tab);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.people));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.chat));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.people));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.people));
+        viewPager.setOffscreenPageLimit(4);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() { //없으면 연결이 안됨
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.people:
-                viewPager.setCurrentItem(0);
-                break;
-            case R.id.chat:
-                viewPager.setCurrentItem(1);
-                break;
-            case R.id.three:
-                viewPager.setCurrentItem(2);
-                break;
-            case R.id.four:
-                viewPager.setCurrentItem(3);
-                break;
-        }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    private void Init(){
+
     }
     private class PagerAdapter extends FragmentStatePagerAdapter{
         public PagerAdapter(FragmentManager fm) {

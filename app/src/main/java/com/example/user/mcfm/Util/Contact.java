071b1c83f4b0f5e113f2952cb4935cc42cf5977e
choi <chl8263@naver.com>
2017-10-18 17:12:59 +0900

    채팅보완

--- app/src/main/java/com/example/user/mcfm/Util/Contact.java
+++ app/src/main/java/com/example/user/mcfm/Util/Contact.java
@@ -26,6 +26,6 @@ public class Contact {
     public static String mento_Type_two="다문화 가정자녀 학습 멘토링";
 
     public static String mento_introduce_one="저는 결혼,이민자 멘토링을 희망하며 한국에 온지는 2달되었습니다. 좋은 멘토 선생님을 원합니다.";
-    public static String mento_introduce_two="";
+    public static String mento_introduce_two="저는 다문화 자녀 학습 멘토링을 원합니다. 저는 국어,수학 학습을 원합니다.";
 
 }

aa012303f43cf49704329bf554cc21fdc569224c
leeyunjea <yunjea0312@naver.com>
2017-10-21 23:36:46 +0900

    aa

---
+++ app/src/main/java/com/example/user/mcfm/Report/TutorReportActivity.java
@@ -0,0 +1,146 @@
+package com.example.user.mcfm.Report;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.user.mcfm.Dialog.ReportSubmitDialog;
+import com.example.user.mcfm.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+//import android.support.annotation.RequiresApi;
+
+public class TutorReportActivity extends AppCompatActivity {
+
+    TextView date_text;
+    ImageView imageView;
+    int REQEUST_OK = 101;
+    FloatingActionButton submit;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.submit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(getApplicationContext(), ReportSubmitDialog.class);
+        startActivity(intent);
+        /*switch (item.getItemId()) {
+            case R.id.quick_action1:
+                //
+                return true;
+            case R.id.action_settings:
+                //
+                return true;
+            case R.id.action_subactivity:
+                startActivity(new Intent(this, SubActivity.class));
+                return true;
+            case R.id.action_navdrawer:
+                startActivity(new Intent(this, NavDrawerActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }*/
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_report);
+
+        Calendar calendar = Calendar.getInstance();
+        String timestr = calendar.getTime().toString();
+
+
+        date_text = (TextView)findViewById(R.id.date_text);
+        date_text.setText(getDateString());
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQEUST_OK);
+            }
+        });
+
+       /* submit = (FloatingActionButton)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReportSubmitDialog.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(getBaseContext(), "resultCode : "+resultCode,Toast.LENGTH_SHORT).show();
+
+        if(requestCode==REQEUST_OK) {
+            if(resultCode== Activity.RESULT_OK) {
+                try {
+                    String picture_name = getImageNameToUri(data.getData());
+
+                    Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                    imageView.setImageBitmap(image_bitmap);
+
+                }catch(FileNotFoundException e) { e.printStackTrace(); } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getImageNameToUri(Uri data)
+    {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        String imgPath = cursor.getString(column_index);
+        String imgName = imgPath.substring(imgPath.lastIndexOf("/")+1);
+
+        return imgName;
+    }
+
+    public String getDateString()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+        String str_date = df.format(new Date());
+
+        return str_date;
+    }
+
+}

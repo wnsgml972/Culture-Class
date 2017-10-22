8ed416a4a303c44aea5c7875419b8e2ec8e08bde
junhee <wnsgml972@naver.com>
2017-10-22 18:51:28 +0900

    junhee

--- app/src/main/java/com/example/user/mcfm/Report/TutorReportActivity.java
+++ app/src/main/java/com/example/user/mcfm/Report/TutorReportActivity.java
@@ -11,11 +11,10 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,21 +37,22 @@ public class TutorReportActivity extends AppCompatActivity {
     ImageView imageView;
     int REQEUST_OK = 101;
     FloatingActionButton submit;
+    ImageButton exitbtn;
+    EditText week;
+    EditText tutor;
+    EditText group;
+    EditText tutee;
+    EditText active;
+
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.submit_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    protected void onResume() {
+        super.onResume();
+        if(SubmitReportDialog.submit_check == true) {
+            SubmitReportDialog.submit_check = false;
+            finish();
+        }
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent = new Intent(getApplicationContext(), SubmitReportDialog.class);
-        startActivity(intent);
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +61,7 @@ public class TutorReportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tutor_report);
         Calendar calendar = Calendar.getInstance();
         String timestr = calendar.getTime().toString();
+
 
 
         date_text = (TextView)findViewById(R.id.date_text);
@@ -74,6 +75,49 @@ public class TutorReportActivity extends AppCompatActivity {
                 intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
                 intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, REQEUST_OK);
+            }
+        });
+
+        week = (EditText)findViewById(R.id.week);
+        tutor = (EditText)findViewById(R.id.tutor);
+        group = (EditText)findViewById(R.id.group);
+        tutee = (EditText)findViewById(R.id.tutee);
+        active = (EditText)findViewById(R.id.active);
+
+        submit = (FloatingActionButton)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(week.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "주차를 작성하지 않으셨습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(tutor.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "튜터를 작성하지 않으셨습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(group.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "소속을 작성하지 않으셨습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(tutee.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "튜티를 작성하지 않으셨습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(active.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "활동내용을 작성하지 않으셨습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getApplicationContext(), SubmitReportDialog.class);
+                startActivity(intent);
+            }
+        });
+
+        exitbtn = (ImageButton)findViewById(R.id.report_exit_btn);
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
         initStatusbar();
@@ -91,8 +135,6 @@ public class TutorReportActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        Toast.makeText(getBaseContext(), "resultCode : "+resultCode,Toast.LENGTH_SHORT).show();
 
         if(requestCode==REQEUST_OK) {
             if(resultCode== Activity.RESULT_OK) {

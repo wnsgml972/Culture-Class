bd4d5423f217d2b838977de223bb1e1e517db948
leeyunjea <yunjea0312@naver.com>
2017-10-22 18:55:19 +0900

    Merge branch 'master' of C:\seoul_contest\MCFM with conflicts.

--- app/src/main/java/com/example/user/mcfm/Adapter/Fourth_RecyclerView_Adapter.java
+++ app/src/main/java/com/example/user/mcfm/Adapter/Fourth_RecyclerView_Adapter.java
@@ -3,17 +3,14 @@ package com.example.user.mcfm.Adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.user.mcfm.Adapter_Item.Fourth_RecyclerView_Item;
-
 import com.example.user.mcfm.Dialog.Fourth_Recycler_Item_Developer_Dialog;
 import com.example.user.mcfm.Dialog.Fourth_Recycler_Item_I_Dialog;
 import com.example.user.mcfm.Dialog.Fourth_Recycler_Item_QNA_Dialog;

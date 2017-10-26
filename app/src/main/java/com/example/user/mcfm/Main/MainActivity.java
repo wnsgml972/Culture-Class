91610ff45c2e68fe4ee3412b84dd972d9f374311
User <chl8263@naver.com>
2017-10-27 02:54:13 +0900

    달력완성

--- app/src/main/java/com/example/user/mcfm/Main/MainActivity.java
+++ app/src/main/java/com/example/user/mcfm/Main/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
         initViewPager();
         initStatusbar();
